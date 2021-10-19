00fbd79d760264efb1a0e34e5a3be01f42301510
Oseas Moreto <oseas_moreto@hotmail.com>
2021-10-19 15:14:07 -0300

    Criação do perfil de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,16 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { esureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
 router.post("/messages", esureAuthenticated, new CreateMessageController().handle);
-router.get("/messages/last3", new GetLast3MessagesController().handle)
+router.get("/messages/last3", new GetLast3MessagesController().handle);
+
+router.get("/profile", esureAuthenticated, new ProfileUserController().handle);
 
 export { router };
